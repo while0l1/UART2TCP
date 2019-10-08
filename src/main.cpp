64dee692ac968e1681d6bedfc294aa2048462b89
Farry <newfarry@126.com>
2019-10-08 21:59:22 +0800

    update for OpenIoTHub

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,29 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 
 #define SYSRuntime D4
 
-String ssid = "UART2TCP"; //WiFi名称
-String password = "1234567890"; //WiFi密码
-
-IPAddress mylocalIP(192, 168, 5, 2); //本地IP地址
-IPAddress mygateWay(192, 168, 5, 1); //网关
-IPAddress mysubnet(255, 255, 255, 0); //子网掩码
 int tcpPort = 8266; //服务器端口
 char valFromPC; //接收TCP发过来的数据
 String valFromMCU = ""; //接收串口发过来的数据
 int exitFlag; //退出配置模式标志
-unsigned int Baud = 460800; //串口波特率
+unsigned int Baud = 115200; //串口波特率
 
 WiFiServer server(tcpPort);
 WiFiClient client;
 
-WiFiServer server2(8267);
-WiFiClient client2;
 //开启热点
 //开启TCP服务
 void StartServer()
 {
-    WiFi.mode(WIFI_AP); //AP模式（仅打开热点）
-    WiFi.softAPConfig(mylocalIP, mygateWay, mysubnet); //配置热点
-    WiFi.softAP(ssid, password, 1, 0, 10); //设置SSID，密码，最大连接数等
-
-    //输出配置信息
-    Serial.println("");
-    Serial.println("WiFi Created!");
-    Serial.print("SSID: ");
-    Serial.println(ssid);
-    Serial.print("Password: ");
-    Serial.println(password);
-    Serial.println("IP address is: 192.168.5.2");
-    Serial.print("Server port is: ");
-    Serial.println(tcpPort);
+    // 连接WIFI
+    WiFi.mode(WIFI_STA);
+    // 选取一种连接路由器的方式 
+    // WiFi.begin(ssid, password);
+    WiFi.beginSmartConfig();
 
     server.begin(tcpPort); //开启TCP服务器
     server.setNoDelay(true);
-    server2.begin(8267);
-    server2.setNoDelay(true);
-}
-
-//配置模式
-void SettingMode()
-{
-    exitFlag = 1;
-    Serial.println("-----------------UART to TCP Beta--------------------");
-    Serial.println("You can type \"HELP\" for help");
-    Serial.print('>');
-    while (exitFlag == 1) {
-        while (Serial.available()) {
-            valFromMCU = Serial.readString(); //读取串口数据
-            if (valFromMCU.substring(0, 5) == "SSID:") { //修改热点名称
-                ssid = valFromMCU.substring(5);
-                Serial.print('>');
-            } else if (valFromMCU.substring(0, 4) == "PSW:") { //修改热点密码
-                password = valFromMCU.substring(4);
-                Serial.print('>');
-            } else if (valFromMCU.substring(0, 5) == "PORT:") { //修改端口
-                tcpPort = valFromMCU.substring(5).toInt();
-                Serial.print('>');
-            } else if (valFromMCU.substring(0, 5) == "BAUD:") { //修改波特率
-                Baud = valFromMCU.substring(5).toInt();
-            } else if (valFromMCU.substring(0, 4) == "HELP") { //打印帮助信息
-                Serial.println("Example:");
-                Serial.println("Set you SSID name as UART2TCP:   SSID:UART2TCP");
-                Serial.println("Set you password as 1234567890:  PSW:1234567890");
-                Serial.println("Set you TCP port as 8266:        PORT:8266");
-                Serial.println("Set you baudrate as 9600:        BAUD:9600");
-                Serial.println("Exit setting mode:               EXIT");
-            } else if (valFromMCU.substring(0, 4) == "EXIT") { //退出配置
-                exitFlag = 0;
-            }
-        }
-    }
-    StartServer(); //重新启动服务
 }
 
 void setup()
@@ -90,6 +36,16 @@ void setup()
     delay(10);
     //启动TCP服务
     StartServer();
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        // Serial.print(".");
+    }
+
+    if (MDNS.begin("esp-switch-"+WiFi.macAddress())) {
+        MDNS.addService("iotdevice", "tcp", tcpPort);
+    }
 }
 
 void SysRunningLED()
@@ -106,20 +62,11 @@ void SysRunningLED()
 
 void loop()
 {
+    MDNS.update();
     SysRunningLED();
     if (server.hasClient()) { //有新的设备连接上
         client = server.available();
         Serial.println("client connected!");
-    }
-    if (server2.hasClient()) {
-        client2 = server2.available();
-        Serial.println("client2 connected!");
-    }
-
-    while (client2.available()) {
-        digitalWrite(SYSRuntime, LOW);
-        valFromPC = client2.read();
-        Serial.print(valFromPC);
     }
     while (client.available()) //TCP接收单片机发过来的信息，串口发给电脑
     {
@@ -131,12 +78,6 @@ void loop()
     while (Serial.available()) { //串口接收电脑发送过来的信息，TCP发送给单片机
         digitalWrite(SYSRuntime, LOW);
         valFromMCU = Serial.readString();
-        if (valFromMCU.substring(0, 8) == ":Setting") { //进入配置模式
-            Serial.println("Entering setting mode");
-            SettingMode();
-        } else {
-            client2.print(valFromMCU);
-            client.print(valFromMCU); //把串口数据通过TCP发出去
-        }
+        client.print(valFromMCU); //把串口数据通过TCP发出去
     }
 }
