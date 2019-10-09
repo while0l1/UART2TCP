f4dfef3458f24946212783293db9bc05c28ca79f
Farry <newfarry@126.com>
2019-10-09 14:31:15 +0800

    通过mdns声明设备信息

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,18 @@ void setup()
 
     MDNS.begin("esp-switch-"+WiFi.macAddress());
     MDNS.addService("iotdevice", "tcp", tcpPort);
-    MDNS.addServiceTxt("iotdevice", "tcp", "mykEY","myvALUE");
+    // MDNS.addServiceTxt("iotdevice", "tcp", "mykEY","myvALUE");
+    MDNS.addServiceTxt("iotdevice", "tcp", "name", "串口转TCP");
+    MDNS.addServiceTxt("iotdevice", "tcp", "model", "com.iotserv.devices.UART2TCP");
+    MDNS.addServiceTxt("iotdevice", "tcp", "mac", WiFi.macAddress());
+    MDNS.addServiceTxt("iotdevice", "tcp", "id", ESP.getSketchMD5());
+    MDNS.addServiceTxt("iotdevice", "tcp", "ui-support", "web,native");
+    MDNS.addServiceTxt("iotdevice", "tcp", "ui-first", "web");
+    MDNS.addServiceTxt("iotdevice", "tcp", "author", "Farry");
+    MDNS.addServiceTxt("iotdevice", "tcp", "email", "newfarry@126.com");
+    MDNS.addServiceTxt("iotdevice", "tcp", "home-page", "https://github.com/iotdevice");
+    MDNS.addServiceTxt("iotdevice", "tcp", "firmware-respository", "https://github.com/iotdevice/ESP32-CAM");
+    MDNS.addServiceTxt("iotdevice", "tcp", "firmware-version", "1.0");
 }
 
 void loop()
